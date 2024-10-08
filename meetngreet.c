e2936a559cad7e846157921ac368da76c6ad0166
anf34 <58158072+anf34@users.noreply.github.com>
2024-10-08 21:20:21 +1100

    Add files via upload

---
+++ meetngreet.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hiii\n");
+    char name[50];
+    printf("What is your name?\n");
+    scanf("%s",name);
+    printf("What is up %s\n", name);
+    return 0;
+}
+
+/*
+
+Attempt 1
+#include <stdio.h>
+
+int main() {
+    printf("Hiii");
+    char name[50];
+    name = scanf("What is your name?");
+    printf("What is up %s", name)
+    return 0;
+}
+
+Attempt 2
+#include <stdio.h>
+
+int main() {
+    printf("Hiii");
+    char name[50];
+    printf("What is your name?\n")
+    scanf("%s",name);
+    printf("What is up %s", name);
+    return 0;
+}
+
+Attempt 3
+#include <stdio.h>
+
+int main() {
+    printf("Hiii");
+    char name[50];
+    printf("What is your name?\n");
+    scanf("%s",name);
+    printf("What is up %s", name);
+    return 0;
+}
+
+Attempt 4 is the final
+*/
